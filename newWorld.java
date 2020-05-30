2e0c693375abeefaffe117f45652e29cb81e27c2
meetkataria03 <66174749+meetkataria03@users.noreply.github.com>
2020-05-30 21:27:21 +0530

    Added a for loop.
    I had added a for loop.Please accept it if you like it.

--- newWorld.java
+++ newWorld.java
@@ -2,7 +2,7 @@ class newWorld{
   public static void main(String []args){
     System.out.println("Hello newWorld");
     
-    for(int i=0;i<=4;i++){
+    for(int i=1;i<=5;i++){
       System.out.println(i);
     }
   }
